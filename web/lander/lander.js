886fb6d0dca0e08503fa2e2acbaa4a33753afbed
Simon Jenkins <simon@sdjenkins.com>
2025-04-01 00:23:11 +0100

    lander tweeks II

--- web/lander/lander.js
+++ web/lander/lander.js
@@ -1,18 +1,29 @@
+
+// ----------------------------------------------
 function myMove() {
+    //
+    function frame() {
+
+        if (pos == 750) {
+          clearInterval(id);
+          return;
+        } 
+        if (pos == 350) {
+            clearInterval(id);
+            id = setInterval(frame, 10);
+        }
+        pos++; 
+        dash.innerText = pos;
+        elem.style.top = pos + "px";  
+    }
+    const butt = document.getElementById("butt");
     let id = null;
-    const elem = document.getElementById("animate");   
+    const elem = document.getElementById("ship");
+    const dash = document.getElementById("dash");      
     let pos = 0;
     elem.style.left = "100px";
     elem.style.visibility = "visible";
     clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-      if (pos == 750) {
-        clearInterval(id);
-      } else {
-        pos++; 
-        elem.style.top = pos + "px"; 
-        // 
-      }
-    }
+    id = setInterval(frame, 50);
+
   }
